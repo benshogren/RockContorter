e2c3240d22b01efd68193967af7ba2dea51fc02b
Benjamin Shogren <benshogren@gmail.com>
2012-07-11 21:19:47 -0400

    fixing the monster move to actually work, and adding a gitignore

--- src/RockContorter/DYING_ROCK.java
+++ src/RockContorter/DYING_ROCK.java
@@ -14,17 +14,15 @@ public class Dying_Rock implements BoardPiece {
 
     @Override
     public void Update(Board board, Point PlayerPosition) {
-
-        if (here == new Point(PlayerPosition.x, PlayerPosition.y + 1)){
+        if (here.equals(new Point(PlayerPosition.x, PlayerPosition.y + 1))){
             return;
-        } else if (here == new Point(PlayerPosition.x, PlayerPosition.y - 1)){
+        } else if (here.equals(new Point(PlayerPosition.x, PlayerPosition.y - 1))){
             return;
-        } else if (here == new Point(PlayerPosition.x + 1, PlayerPosition.y)) {
+        } else if (here.equals(new Point(PlayerPosition.x + 1, PlayerPosition.y))) {
             return;
-        } else if (here == new Point(PlayerPosition.x - 1, PlayerPosition.y)){
+        } else if (here.equals(new Point(PlayerPosition.x - 1, PlayerPosition.y))){
             return;
         } else {
-
             lifeSpan = lifeSpan + 1;
 
             if (lifeSpan == 15){
