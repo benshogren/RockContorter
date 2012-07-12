7a64e21624de00761d9a5047c83f3030730e5f81
Benjamin Shogren <benshogren@gmail.com>
2012-07-12 16:04:35 -0400

    RangedMonsters now move to a certain distance away from player and open fire.

--- src/RockContorter/RangedMonsters.java
+++ src/RockContorter/RangedMonsters.java
@@ -16,74 +16,73 @@ public class RangedMonsters implements BoardPiece{
     @Override
     public void Update(Board board, Point PlayerPosition) {
 
-        if((PlayerPosition.x > here.x) && (PlayerPosition.y == here.y)){
-            board.BoardGrid.put(new Point(here.x + 1, here.y), new FlyingRock(new Point(here.x + 1, here.y), Game.Direction.RIGHT));
 
-        } else if((PlayerPosition.x < here.x) && (PlayerPosition.y == here.y)){
-            board.BoardGrid.put(new Point(here.x - 1, here.y), new FlyingRock(new Point(here.x - 1, here.y), Game.Direction.LEFT));
-
-        } else if((PlayerPosition.x == here.x) && (PlayerPosition.y > here.y)){
-            board.BoardGrid.put(new Point(here.x, here.y + 1), new FlyingRock(new Point(here.x, here.y + 1), Game.Direction.DOWN));
-
-        } else if((PlayerPosition.x > here.x) && (PlayerPosition.y == here.y)){
-            board.BoardGrid.put(new Point(here.x, here.y - 1), new FlyingRock(new Point(here.x, here.y - 1), Game.Direction.RIGHT));
-
-        } else {
+        Point moveUp = new Point(here.x, here.y - 1);
+        if (here.y > PlayerPosition.y + 5 && !board.WallInTheWay(moveUp) && !board.StaticRockInTheWay(moveUp) && !board.DisappearingRockInTheWay(moveUp)){
+            moveToNewPosition(board, moveUp);
+            return;
+        }
+        Point moveDown = new Point(here.x, here.y + 1);
+        if (here.y < PlayerPosition.y - 5 && !board.WallInTheWay(moveDown) && !board.StaticRockInTheWay(moveDown) && !board.DisappearingRockInTheWay(moveDown)){
+            moveToNewPosition(board, moveDown);
+            return;
+        }
+        Point moveLeft = new Point(here.x - 1, here.y);
+        if (here.x > PlayerPosition.x && !board.WallInTheWay(moveLeft) && !board.StaticRockInTheWay(moveLeft) && !board.DisappearingRockInTheWay(moveLeft)){
+            moveToNewPosition(board, moveLeft);
+            return;
+        }
+        Point moveRight = new Point(here.x + 1, here.y);
+        if (here.x < PlayerPosition.x && !board.WallInTheWay(moveRight) && !board.StaticRockInTheWay(moveRight) && !board.DisappearingRockInTheWay(moveRight)){
+            moveToNewPosition(board, moveRight);
             return;
         }
 
-
-
-
-
-
-
-
-
-
-
-//
-//        updateCount++;
-//        if  (updateCount % 2 == 0) {
-//            Point moveUp = new Point(here.x, here.y - 1);
-//            if (here.y > PlayerPosition.y && !board.WallInTheWay(moveUp) && !board.RockInTheWay(moveUp)){
-//                moveToNewPosition(board, moveUp);
-//                return;
-//            }
-//            Point moveDown = new Point(here.x, here.y + 1);
-//            if (here.y < PlayerPosition.y && !board.WallInTheWay(moveDown) && !board.RockInTheWay(moveDown)){
-//                moveToNewPosition(board, moveDown);
-//                return;
-//            }
-//            Point moveLeft = new Point(here.x - 1, here.y);
-//            if (here.x > PlayerPosition.x && !board.WallInTheWay(moveLeft) && !board.RockInTheWay(moveLeft)){
-//                moveToNewPosition(board, moveLeft);
-//                return;
-//            }
-//            Point moveRight = new Point(here.x + 1, here.y);
-//            if (here.x < PlayerPosition.x && !board.WallInTheWay(moveRight) && !board.RockInTheWay(moveRight)){
-//                moveToNewPosition(board, moveRight);
-//                return;
-//            }
-//
 ////            // back-up random moves for stuck monsters
-//            java.util.List<Point> moves = new ArrayList<Point>();
-//            moves.add(new Point(here.x - 1, here.y));
-//            moves.add(new Point(here.x + 1, here.y));
-//            moves.add(new Point(here.x, here.y - 1));
-//            moves.add(new Point(here.x, here.y + 1));
+//        java.util.List<Point> moves = new ArrayList<Point>();
+//        moves.add(new Point(here.x - 1, here.y));
+//        moves.add(new Point(here.x + 1, here.y));
+//        moves.add(new Point(here.x, here.y - 1));
+//        moves.add(new Point(here.x, here.y + 1));
 //
-//            java.util.List<Point> validMoves = new ArrayList<Point>();
-//            for (Point move : moves) {
-//                if (!board.WallInTheWay(move)) {
-//                    validMoves.add(move);
-//                }
-//            }
-//            if (validMoves.size() > 0) {
-//                here = validMoves.get((int) (Math.random()*validMoves.size()));
+//        java.util.List<Point> validMoves = new ArrayList<Point>();
+//        for (Point move : moves) {
+//            if (!board.WallInTheWay(move) && !board.DisappearingRockInTheWay(move) && !board.StaticRockInTheWay(move)) {
+//                validMoves.add(move);
 //            }
 //        }
+//        if (validMoves.size() > 0) {
+//            here = validMoves.get((int) (Math.random()*validMoves.size()));
+//        }
+
+
+
+
+        updateCount = updateCount + 1;
+        if  (updateCount == 4) {
+            updateCount = 0;
+
+            if((PlayerPosition.x > here.x) && (PlayerPosition.y == here.y)){
+                board.BoardGrid.put(new Point(here.x + 1, here.y), new FlyingRock(new Point(here.x + 1, here.y), Game.Direction.RIGHT));
+            } else if((PlayerPosition.x < here.x) && (PlayerPosition.y == here.y)){
+                board.BoardGrid.put(new Point(here.x - 1, here.y), new FlyingRock(new Point(here.x - 1, here.y), Game.Direction.LEFT));
+            } else if((PlayerPosition.x == here.x) && (PlayerPosition.y > here.y)){
+                board.BoardGrid.put(new Point(here.x, here.y + 1), new FlyingRock(new Point(here.x, here.y + 1), Game.Direction.DOWN));
+            } else if((PlayerPosition.x > here.x) && (PlayerPosition.y == here.y)){
+                board.BoardGrid.put(new Point(here.x, here.y - 1), new FlyingRock(new Point(here.x, here.y - 1), Game.Direction.RIGHT));
+            } else {
+                return;
+            }
+        }
     }
+
+
+
+
+
+
+
+
 
     private void moveToNewPosition(Board board, Point moveLeft) {
         board.BoardGrid.put(here, new Empty());
