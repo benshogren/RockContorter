cb60a9c44457c7ceea6289392f73dd86c5e9b29d
Benjamin Shogren <benshogren@gmail.com>
2012-07-14 13:35:03 -0400

    renaming piece

--- src/RockContorter/EmptyPiece.java
+++ src/RockContorter/EmptyPiece.java
@@ -4,6 +4,7 @@ import java.awt.*;
 
 public class Empty implements BoardPiece {
 
+    
     @Override
     public void Update(Board board, Point PlayerPosition) {
         //To change body of implemented methods use File | Settings | File Templates.
